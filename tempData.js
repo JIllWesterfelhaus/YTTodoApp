4d6c0c0c91677bd23fea383c76536eea1256de17
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-09-03 15:02:48 -0500

    staging changes

---
+++ tempData.js
@@ -0,0 +1,46 @@
+//Temporary Data until we connect with Firebase
+
+export default tempData = [
+    {
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Pick Up Laundry",
+                completed: true
+            },
+            {
+                title: "Take Out Trash",
+                completed: true,
+            },
+            {
+                title: "Wash Dishes",
+                completed: false
+            }]
+    }]
